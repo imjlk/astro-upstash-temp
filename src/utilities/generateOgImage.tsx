d58b9f01836b2a572322cc71b999b64a9aa5729d
imjlk <i@m.junglei.kim>
2023-02-17 05:05:41 +0900

    fix

--- src/utilities/generateOgImage.tsx
+++ src/utilities/generateOgImage.tsx
@@ -1,14 +1,6 @@
 import satori, { SatoriOptions } from 'satori';
 // import { SITE } from '@config';
 
-const fetchFont = async () => {
-	const fontFile = await fetch('https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf');
-	const fontData: ArrayBuffer = await fontFile.arrayBuffer();
-	return fontData;
-};
-
-const fontData = await fetchFont();
-
 const ogImage = (text = 'hello') => {
 	return (
 		<div style={{ color: 'black', display: 'flex' }}>
@@ -18,19 +10,30 @@ const ogImage = (text = 'hello') => {
 	);
 };
 
-const options: SatoriOptions = {
-	width: 600,
-	height: 400,
-	fonts: [
-		{
-			name: 'IBM Plex Mono',
-			data: fontData,
-			weight: 400,
-			style: 'normal',
-		},
-	],
-};
+const generateOgImage = async (mytext: string) => {
+	const fetchFont = async () => {
+		const fontFile = await fetch(
+			'https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf',
+		);
+		const fontData: ArrayBuffer = await fontFile.arrayBuffer();
+		return fontData;
+	};
 
-const generateOgImage = async (mytext: string) => await satori(ogImage(mytext), options);
+	const fontData = await fetchFont();
+
+	const options: SatoriOptions = {
+		width: 1200,
+		height: 630,
+		fonts: [
+			{
+				name: 'IBM Plex Mono',
+				data: fontData,
+				weight: 400,
+				style: 'normal',
+			},
+		],
+	};
+	return await satori(ogImage(mytext), options);
+};
 
 export default generateOgImage;
